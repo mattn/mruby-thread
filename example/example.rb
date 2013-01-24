90d59f43dc2b79852e4ec605407a760c4d20206f
mattn <mattn.jp@gmail.com>
2013-01-24 15:58:22 +0900

    fix example.

--- example/example.rb
+++ example/example.rb
@@ -5,7 +5,9 @@ th = Thread.new(v) do |x|
   puts "in thread: x is #{x}"
   puts "in thread: v is #{v.inspect}" # should be nil
   v = "bar" # shouldn't be updated
+  "baz"
 end
 
-th.join
+r = th.join
 puts "in main: v is #{v}" # should be remained as foo
+puts "in main: r is #{r}" # should be remained as baz
