94a6719c82f28ba2669195474b0b6f92feea7541
mattn <mattn.jp@gmail.com>
2013-01-24 15:54:00 +0900

    migrate values.

--- src/mrb_thread.c
+++ src/mrb_thread.c
@@ -1,5 +1,6 @@
 #include <mruby.h>
 #include <mruby/string.h>
+#include <mruby/array.h>
 #include <mruby/proc.h>
 #include <mruby/data.h>
 #include <mruby/variable.h>
@@ -14,16 +15,72 @@ typedef struct {
   mrb_value* argv;
   struct RProc* proc;
   pthread_t thread;
+  mrb_state* mrb;
+  mrb_value result;
 } mrb_thread_context;
 
 static void
 mrb_thread_context_free(mrb_state *mrb, void *p) {
+  mrb_thread_context* context = (mrb_thread_context*) p;
+  if (context->mrb) mrb_close(context->mrb);
   free(p);
 }
 
 static const struct mrb_data_type mrb_thread_context_type = {
   "mrb_thread_context", mrb_thread_context_free,
 };
+
+// based on https://gist.github.com/3066997
+static mrb_value
+migrate_simple_value(mrb_state *mrb, mrb_value v, mrb_state *mrb2) {
+  mrb_value nv;
+  const char *s;
+  int len;
+
+  nv.tt = v.tt;
+  switch (mrb_type(v)) {
+  case MRB_TT_FALSE:
+  case MRB_TT_TRUE:
+  case MRB_TT_FIXNUM:
+    nv.value.i = v.value.i;
+    break;
+  case MRB_TT_SYMBOL:
+    nv = mrb_symbol_value(mrb_intern_str(mrb2, v));
+    break;
+  case MRB_TT_FLOAT:
+    nv.value.f = v.value.f;
+    break;
+  case MRB_TT_STRING:
+    {
+      struct RString *str = mrb_str_ptr(v);
+
+      s = str->ptr;
+      len = str->len;
+      nv = mrb_str_new(mrb2, s, len);
+    }
+    break;
+  case MRB_TT_ARRAY:
+    {
+      struct RArray *a0, *a1;
+      int i;
+
+      a0 = mrb_ary_ptr(v);
+      nv = mrb_ary_new_capa(mrb2, a0->len);
+      a1 = mrb_ary_ptr(nv);
+      for (i=0; i<a0->len; i++) {
+        int ai = mrb_gc_arena_save(mrb2);
+        a1->ptr[i] = migrate_simple_value(mrb, a0->ptr[i], mrb2);
+        a1->len++;
+        mrb_gc_arena_restore(mrb2, ai);
+      }
+    }
+    break;
+  default:
+    mrb_raise(mrb, E_TYPE_ERROR, "cannot migrate object");
+    break;
+  }
+  return nv;
+}
 
 static void*
 mrb_thread_func(void* data) {
@@ -32,12 +89,12 @@ mrb_thread_func(void* data) {
   int i;
   for (i = 0; i < context->argc; i++) {
     if (mrb_special_const_p(context->argv[i])) {
-      context->argv[i] = mrb_obj_clone(mrb, context->argv[i]);
+      context->argv[i] = migrate_simple_value(context->mrb, context->argv[i], mrb);
     }
   }
   struct RProc* np = mrb_proc_new(mrb, context->proc->body.irep);
-  mrb_yield_argv(mrb, mrb_obj_value(np), context->argc, context->argv);
-  mrb_close(mrb);
+  context->result = mrb_yield_argv(mrb, mrb_obj_value(np), context->argc, context->argv);
+  context->mrb = mrb;
   return NULL;
 }
 
@@ -52,6 +109,8 @@ mrb_thread_init(mrb_state* mrb, mrb_value self) {
     context->proc = mrb_proc_ptr(proc);
     context->argc = argc;
     context->argv = argv;
+    context->mrb = mrb;
+    context->result = mrb_nil_value();
 
     mrb_iv_set(mrb, self, mrb_intern(mrb, "context"), mrb_obj_value(
       Data_Wrap_Struct(mrb, mrb->object_class,
@@ -68,7 +127,11 @@ mrb_thread_join(mrb_state* mrb, mrb_value self) {
   mrb_thread_context* context = NULL;
   Data_Get_Struct(mrb, value_context, &mrb_thread_context_type, context);
   pthread_join(context->thread, NULL);
-  return mrb_nil_value();
+
+  context->result = migrate_simple_value(mrb, context->result, mrb);
+  mrb_close(context->mrb);
+  context->mrb = NULL;
+  return context->result;
 }
 
 void
