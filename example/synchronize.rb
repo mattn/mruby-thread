101cf17ed851d6a9ae0a2823fb51f0e06bfa347b
mattn <mattn.jp@gmail.com>
2014-10-02 15:09:20 +0900

    Add some examples

--- example/synchronize.rb
+++ example/synchronize.rb
@@ -4,17 +4,18 @@ m = Mutex.new
 v = "foo"
 th = Thread.new(v, m) do |v, m|
   for num in 1..3 do
-    m.lock
-    puts "thread: num = #{num}"
-    m.unlock
+    m.synchronize do
+      puts "thread: num = #{num}"
+      m.unlock
+    end
     Thread.sleep 1
   end
   nil
 end
 for num in 1..3 do
-  m.lock
-  puts "main: num = #{num}"
-  m.unlock
+  m.synchronize do
+    puts "main: num = #{num}"
+  end
   Thread.sleep 1
 end
 th.join
